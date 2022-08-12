1198c053edadfeedd1f67cc8459f4b59d6cc5d03
oldjunping <oldjun@sina.com>
2022-08-12 13:14:04 +0800

    pi-lang

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -22,6 +22,8 @@ var builtins = map[string]*object.Builtin{
 				return &object.Integer{Value: int64(len(arg.Value))}
 			case *object.List:
 				return &object.Integer{Value: int64(len(arg.Elements))}
+			case *object.Hash:
+				return &object.Integer{Value: int64(len(arg.Pairs))}
 			default:
 				return newError("argument to `len` not supported, got %s", args[0].Type())
 			}
