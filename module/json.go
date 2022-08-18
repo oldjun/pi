cfcc17d4dc61bdfc07883a295a050f87e0e046e5
oldjunping <oldjun@sina.com>
2022-08-18 09:34:30 +0800

    pi-lang

--- module/json.go
+++ module/json.go
@@ -1,6 +1,9 @@
 package module
 
-import "pilang/object"
+import (
+	"encoding/json"
+	"pilang/object"
+)
 
 // JsonProperties module properties
 var JsonProperties = map[string]object.ModuleProperty{}
@@ -14,9 +17,50 @@ func init() {
 }
 
 func decode(args []object.Object) object.Object {
-	return &object.Null{}
+	var i interface{}
+	input := args[0].(*object.String).Value
+	err := json.Unmarshal([]byte(input), &i)
+	if err != nil {
+		return object.NewError("Error while parsing json: %s", err)
+	}
+	return interfaceToObject(i)
 }
 
 func encode(args []object.Object) object.Object {
+	return &object.Null{}
+}
+
+func interfaceToObject(i interface{}) object.Object {
+	switch v := i.(type) {
+	case map[string]interface{}:
+		hash := &object.Hash{}
+		hash.Pairs = make(map[object.HashKey]object.HashPair)
+		for key, val := range v {
+			pair := object.HashPair{
+				Key:   &object.String{Value: key},
+				Value: interfaceToObject(val),
+			}
+			hash.Pairs[pair.Key.(object.Hashable).HashKey()] = pair
+		}
+		return hash
+	case []interface{}:
+		list := &object.List{}
+		for _, elem := range v {
+			list.Elements = append(list.Elements, interfaceToObject(elem))
+		}
+		return list
+	case string:
+		return &object.String{Value: v}
+	case int64:
+		return &object.Integer{Value: v}
+	case float64:
+		return &object.Float{Value: v}
+	case bool:
+		if v {
+			return &object.Boolean{Value: true}
+		} else {
+			return &object.Boolean{Value: false}
+		}
+	}
 	return &object.Null{}
 }
