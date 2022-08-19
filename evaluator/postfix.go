0df87c63c86a5ef383f1941d9eacfbeb28055c10
oldjunping <oldjun@sina.com>
2022-08-19 19:26:08 +0800

    pi-lang

--- evaluator/postfix.go
+++ evaluator/postfix.go
@@ -10,14 +10,28 @@ func evalPostfix(node *ast.Postfix, env *object.Environment) object.Object {
 	if isError(left) {
 		return left
 	}
-	val := left.(*object.Integer)
-	switch node.Operator {
-	case "++":
-		val = &object.Integer{Value: val.Value + 1}
-	case "--":
-		val = &object.Integer{Value: val.Value - 1}
+	var val object.Object
+	switch left := left.(type) {
+	case *object.Integer:
+		switch node.Operator {
+		case "++":
+			val = &object.Integer{Value: left.Value + 1}
+		case "--":
+			val = &object.Integer{Value: left.Value - 1}
+		default:
+			return object.NewError("postfix operator not support: %s", node.Operator)
+		}
+	case *object.Float:
+		switch node.Operator {
+		case "++":
+			val = &object.Float{Value: left.Value + 1}
+		case "--":
+			val = &object.Float{Value: left.Value - 1}
+		default:
+			return object.NewError("postfix operator not support: %s", node.Operator)
+		}
 	default:
-		return nil
+		return object.NewError("postfix not support: %s", left.Type())
 	}
 	switch nodeLeft := node.Left.(type) {
 	case *ast.Identifier:
@@ -28,7 +42,5 @@ func evalPostfix(node *ast.Postfix, env *object.Environment) object.Object {
 	case *ast.PropertyExpression:
 		return evalPropertyAssignment(nodeLeft, val, env)
 	}
-	//// otherwise
-	//env.Set(node.Left.String(), val)
 	return NULL
 }
