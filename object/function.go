eea4f15408d4e41a9d060f42a7b206a808c68df1
oldjunping <oldjun@sina.com>
2022-08-20 15:50:06 +0800

    pi-lang

--- object/function.go
+++ object/function.go
@@ -19,34 +19,3 @@ func (f *Function) Type() Type { return FUNCTION }
 func (f *Function) String() string {
 	return fmt.Sprintf("<func:%s>", f.Name)
 }
-
-/*
-func (f *Function) String() string {
-	var out bytes.Buffer
-	var params []string
-	for _, p := range f.Parameters {
-		if e, ok := f.Defaults[p.String()]; ok {
-			params = append(params, p.String()+"="+e.String())
-		} else {
-			params = append(params, p.String())
-		}
-	}
-	if f.Args != nil {
-		params = append(params, token.ASTERISK+f.Args.String())
-	}
-	if f.KwArgs != nil {
-		params = append(params, token.ASTERISK_ASTERISK+f.KwArgs.String())
-	}
-	out.WriteString("func")
-	if f.Name != "" {
-		out.WriteString(" " + f.Name)
-	}
-	out.WriteString("(")
-	out.WriteString(strings.Join(params, ", "))
-	out.WriteString(") ")
-	out.WriteString("{ ")
-	out.WriteString(f.Body.String())
-	out.WriteString(" }")
-	return out.String()
-}
-*/
