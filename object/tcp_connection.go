69aa539f50f52a615a7cd30409f04c27f201771a
oldjunping <oldjun@sina.com>
2022-08-21 09:54:22 +0800

    pi-lang

--- object/tcp_connection.go
+++ object/tcp_connection.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"net"
+	"time"
 )
 
 type TcpConnection struct {
@@ -21,6 +22,12 @@ func (c *TcpConnection) Method(method string, args []Object) Object {
 		return c.read(args)
 	case "send":
 		return c.send(args)
+	case "close":
+		return c.close(args)
+	case "set_keep_alive":
+		return c.setKeepAlive(args)
+	case "set_keep_alive_time":
+		return c.setKeepAliveTime(args)
 	}
 	return NewError("tcp_conn undefined method: %s", method)
 }
@@ -64,4 +71,45 @@ func (c *TcpConnection) send(args []Object) Object {
 		return &Integer{Value: int64(size)}
 	}
 	return NewError("wrong type of arguments. tcp_conn.send(): %s", args[0].Type())
+}
+
+func (c *TcpConnection) close(args []Object) Object {
+	if len(args) != 0 {
+		return NewError("wrong number of arguments. tcp_conn.close() got=%d", len(args))
+	}
+	err := c.Handler.Close()
+	if err != nil {
+		return NewError("tcp_conn.close() error: %s", err.Error())
+	}
+	return &Null{}
+}
+
+func (c *TcpConnection) setKeepAlive(args []Object) Object {
+	if len(args) != 1 {
+		return NewError("wrong number of arguments. tcp_conn.set_keep_alive() got=%d", len(args))
+	}
+	switch arg := args[0].(type) {
+	case *Boolean:
+		err := c.Handler.SetKeepAlive(arg.Value)
+		if err != nil {
+			return NewError("tcp_conn.set_keep_alive() error: %s", err.Error())
+		}
+		return &Null{}
+	}
+	return NewError("wrong type of arguments. tcp_conn.set_keep_alive(): %s", args[0].Type())
+}
+
+func (c *TcpConnection) setKeepAliveTime(args []Object) Object {
+	if len(args) != 1 {
+		return NewError("wrong number of arguments. tcp_conn.set_keep_alive_time() got=%d", len(args))
+	}
+	switch arg := args[0].(type) {
+	case *Integer:
+		err := c.Handler.SetKeepAlivePeriod(time.Second * time.Duration(arg.Value))
+		if err != nil {
+			return NewError("tcp_conn.set_keep_alive_time() error: %s", err.Error())
+		}
+		return &Null{}
+	}
+	return NewError("wrong type of arguments. tcp_conn.set_keep_alive_time(): %s", args[0].Type())
 }
